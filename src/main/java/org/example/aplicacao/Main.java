0f03650cad162b737a2546110e9db52dd8009b14
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-11-12 18:12:39 -0300

    dependencies

--- src/main/java/org/example/aplicacao/Main.java
+++ src/main/java/org/example/aplicacao/Main.java
@@ -1,12 +1,22 @@
 package org.example.aplicacao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.example.dominio.Pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Gabriel Venancio", "gabrielvenancio@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Laiza Caroline", "laizacaroline@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
+        Pessoa p1 = new Pessoa(null, "Gabriel Venancio", "gabrielvenancio@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Laiza Caroline", "laizacaroline@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pessoa-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
     }
 }
