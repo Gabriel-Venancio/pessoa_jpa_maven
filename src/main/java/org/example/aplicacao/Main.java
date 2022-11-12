28c788f9ee0c602a179c91c7cdc5f4a13674ea4d
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-11-12 18:20:47 -0300

    create, read, delete

--- src/main/java/org/example/aplicacao/Main.java
+++ src/main/java/org/example/aplicacao/Main.java
@@ -7,16 +7,28 @@ import org.example.dominio.Pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Gabriel Venancio", "gabrielvenancio@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Laiza Caroline", "laizacaroline@gmail.com");
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pessoa-jpa");
         EntityManager em = emf.createEntityManager();
 
+        Pessoa p1 = new Pessoa(null, "Gabriel Venancio", "gabrielvenancio@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Laiza Caroline", "laizacaroline@gmail.com");
         em.getTransaction().begin();
         em.persist(p1);
         em.persist(p2);
         em.getTransaction().commit();
+
+        Pessoa a = em.find(Pessoa.class, 2);
+        System.out.println(a);
+
+        Pessoa b = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(b);
+        em.getTransaction().commit();
+
         System.out.println("Pronto!");
+
+        em.close();
+        emf.close();
     }
 }
