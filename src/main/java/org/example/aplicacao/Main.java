b5efe3642ea2f8d9e29c9fb8320aed1eef97299e
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-11-12 15:47:23 -0300

    create Pessoa class

--- src/main/java/org/example/aplicacao/Main.java
+++ src/main/java/org/example/aplicacao/Main.java
@@ -1,7 +1,12 @@
-package org.example;
+package org.example.aplicacao;
+
+import org.example.dominio.Pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa p1 = new Pessoa(1, "Gabriel Venancio", "gabrielvenancio@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Laiza Caroline", "laizacaroline@gmail.com");
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
